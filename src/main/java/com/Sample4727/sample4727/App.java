eb3ef43513d77f1ae3fce29338938d28201fc5fd
kalani4727 <kalani4727@gmail.com>
2021-07-15 01:05:34 +0530

    first commit

---
+++ src/main/java/com/Sample4727/sample4727/App.java
@@ -0,0 +1,13 @@
+package com.Sample4727.sample4727;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
